0ef289fd97d6614cb1fd8202ae633367f78c0cc6
EUMC1ST <edgarmc100@gmail.com>
2020-09-26 00:08:41 -0500

    main section modified

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let first = document.getElementById('first');
+let second = document.getElementById('second');
+let third = document.getElementById('third');
+first.addEventListener('click',function () {
+    location.href = 'https://edgarmc100.github.io/Portafolio/'
+})
+
+second.addEventListener('click',function () {
+    location.href = 'https://play.google.com/store/apps/details?id=mx.com.credicapital.bancamovil&hl=en_SG'
+})
+
+third.addEventListener('click',function () {
+    location.href = 'https://edgarmc100.github.io/restaurant-spa/';
+})
