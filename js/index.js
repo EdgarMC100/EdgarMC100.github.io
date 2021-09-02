f5519eb54f9fb923190ca74aed96d8ddf5de7218
edgarmc1 <edgarmc100@gmail.com>
2021-09-01 19:08:51 -0500

    Updates portafolio projects

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
-let first = document.getElementById('first');
-let second = document.getElementById('second');
-let third = document.getElementById('third');
-first.addEventListener('click',function () {
-    location.href = 'https://edgarmc100.github.io/Portafolio/'
-})
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+let third = document.getElementById("third");
+let fourth = document.getElementById("fourth");
+let fifth = document.getElementById("fifth");
+first.addEventListener("click", function () {
+  location.href = "https://edgarmc100.github.io/Portafolio/";
+});
 
-second.addEventListener('click',function () {
-    location.href = 'https://play.google.com/store/apps/details?id=mx.com.credicapital.bancamovil&hl=en_SG'
-})
+second.addEventListener("click", function () {
+  location.href =
+    "https://identity.conekta.com/Account/Register?client_id=f508bb85-1188-4c08-beb3-a076e1d41f9e";
+});
 
-third.addEventListener('click',function () {
-    location.href = 'https://edgarmc100.github.io/restaurant-spa/';
-})
+third.addEventListener("click", function () {
+  location.href = "https://mimonte.montepiedad.com.mx/login";
+});
+
+fourth.addEventListener("click", function () {
+  location.href =
+    "https://play.google.com/store/apps/details?id=mx.com.credicapital.bancamovil&hl=en_SG";
+});
+
+fifth.addEventListener("click", function () {
+  location.href = "https://edgarmc100.github.io/restaurant-spa/";
+});
